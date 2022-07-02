d3bb37d9ecf0925476542c67cd2766fbb82b4c8a
zhanglioncel <zhanglioncel@gmail.com>
2022-07-03 09:44:20 +1200

    update car class

--- ShowcaseProject/src/main/java/com/packt/cardatabase/domain/Car.java
+++ ShowcaseProject/src/main/java/com/packt/cardatabase/domain/Car.java
@@ -7,8 +7,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="carinfor")
 public class Car {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
