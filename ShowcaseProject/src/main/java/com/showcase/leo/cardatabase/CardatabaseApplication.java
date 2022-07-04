91209bc44b3f0e965fb482922b8741b40e23eded
zhanglioncel <zhanglioncel@gmail.com>
2022-07-05 11:28:24 +1200

    update package structures

--- ShowcaseProject/src/main/java/com/showcase/leo/cardatabase/CardatabaseApplication.java
+++ ShowcaseProject/src/main/java/com/showcase/leo/cardatabase/CardatabaseApplication.java
@@ -1,4 +1,4 @@
-package com.packt.cardatabase;
+package com.showcase.leo.cardatabase;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -6,10 +6,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.packt.cardatabase.domain.Car;
-import com.packt.cardatabase.domain.CarRepository;
-import com.packt.cardatabase.domain.Owner;
-import com.packt.cardatabase.domain.OwnerRepository;
+import com.showcase.leo.cardatabase.domain.Car;
+import com.showcase.leo.cardatabase.domain.CarRepository;
+import com.showcase.leo.cardatabase.domain.Owner;
+import com.showcase.leo.cardatabase.domain.OwnerRepository;
 
 @SpringBootApplication
 public class CardatabaseApplication {
--- ShowcaseProject/src/main/java/com/showcase/leo/cardatabase/domain/Owner.java
+++ ShowcaseProject/src/main/java/com/showcase/leo/cardatabase/domain/Owner.java
@@ -1,4 +1,4 @@
-package com.packt.cardatabase.domain;
+package com.showcase.leo.cardatabase.domain;
 
 import java.util.List;
 
