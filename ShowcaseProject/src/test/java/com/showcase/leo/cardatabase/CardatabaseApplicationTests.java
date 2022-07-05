78c7e8f61a5cb037cf82444ca8fead212720a149
zhanglioncel <zhanglioncel@gmail.com>
2022-07-05 16:45:03 +1200

    dd

--- ShowcaseProject/src/test/java/com/showcase/leo/cardatabase/CardatabaseApplicationTests.java
+++ ShowcaseProject/src/test/java/com/showcase/leo/cardatabase/CardatabaseApplicationTests.java
@@ -2,15 +2,27 @@ package com.showcase.leo.cardatabase;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.showcase.leo.cardatabase.domain.Owner;
+import com.showcase.leo.cardatabase.domain.OwnerRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CardatabaseApplicationTests {
-
+		
+	 @Autowired 
+	 private OwnerRepository orepository;
+	 
+	 
 	@Test
 	public void contextLoads() {
+		Owner owner1 = new Owner("Peter" , "Pan");
+		Owner owner2 = new Owner("Jissie" , "Li");
+		orepository.save(owner1);
+		orepository.save(owner2);
 	}
 
 }
